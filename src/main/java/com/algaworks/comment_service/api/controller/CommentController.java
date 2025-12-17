52066ddfe653a6079c86f7fe3a8d386655f5052b
Breno Pedro <brenopedro14@hotmail.com>
2025-12-17 17:53:54 -0300

    get methods

--- src/main/java/com/algaworks/comment_service/api/controller/CommentController.java
+++ src/main/java/com/algaworks/comment_service/api/controller/CommentController.java
@@ -3,6 +3,9 @@ package com.algaworks.comment_service.api.controller;
 import com.algaworks.comment_service.api.model.CommentInput;
 import com.algaworks.comment_service.api.model.CommentOutput;
 import com.algaworks.comment_service.common.IdGenerator;
+import io.hypersistence.tsid.TSID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +14,17 @@ import java.time.OffsetDateTime;
 @RestController
 @RequestMapping("/api/comments")
 public class CommentController {
+
+    @GetMapping
+    public Page<CommentOutput> getComments(Pageable pageable) {
+        return Page.empty();
+    }
+
+    @GetMapping("/{id}")
+    public CommentOutput getSingleComment(@PathVariable TSID id) {
+        return CommentOutput.builder()
+                .author("TESTE").build();
+    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
